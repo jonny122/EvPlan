64157a43d4c231f4abb2e386a4b0fae3bbdebea3
David <vargasd2106@gmail.com>
2017-09-22 20:45:26 -0600

    Arquitectura actualizada

---
+++ Assets/header.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="./Assets/Bootstrap/css/bootstrap.min.css">
+<script src="./Assets/angular.min.js"></script>
+<script src="./Assets/jquery-3.2.1.js"></script>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" ng-app>
+<head>
+	<meta charset="UTF-8">
+	<?php include ("./Assets/header.php");?>
+	<title>EvPlan | Inicio</title>
+</head>
+<body>
+	<header>
+		<h1>Bienvenido a EvPlan tu mejor opción para organizar tus actividades</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="">Inicia sesión</a></li>
+			<li><a href="">Registrate</a></li>
+		</ul>
+	</nav>
+	<section>
+		<img src="" alt="">
+	</section>
+	<aside></aside>
+	<footer></footer>
+</body>
+</html>
